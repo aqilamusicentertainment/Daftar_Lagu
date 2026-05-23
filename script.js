5f0c47e41d391d81ed5de7985c6c498ae0500fd3
Riyan Andhika <128784894+RiyAndhi@users.noreply.github.com>
2026-05-23 18:17:41 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const APP_VERSION =
   "1.1.5";
 
 const SCRIPT_URL =
-  "https://script.google.com/macros/s/AKfycbzkPLEUTH87f-CVRI0rnJbYMVNRmm7Eq4dcwpVf4xTl1-50teSrj6DvgYu17Kz1W6LI/exec";
+  "https://script.google.com/macros/s/AKfycbxHXQUic6pOj1qNJEIv6_NsC0rB6I8OR-WGwJATQajBn2BOnZfP40aqhR87hjR_-Rgw/exec";
 
 let currentRequestPage = 1;
 let currentSongPage = {};
